a49d042c81783f2eb5dcd57a86379e5f0626404a
pepe <leejaesoek@gabia.com>
2024-09-02 15:20:52 +0900

    fonts 추가
    - nerdfonts-symbols
    - hack

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -44,10 +44,9 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("f", "  > Find file", ":cd $HOME/WebstormProjects/ | Telescope find_files<CR>"),
+			dashboard.button("f", "  > Find file", ":cd $PWD | Telescope find_files<CR>"),
 			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
 			dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-			dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 		}
 
 		-- Send config to alpha
